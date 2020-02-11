8546da7c49733ee59aa07d4902b33b70b3acd7a3
MarcinBalejko <mbbm9@o2.pl>
2020-02-11 21:38:53 +0100

    :white_check_mark: Added HtmlWebpackPlugin

--- webpack_demo_app/webpack.config.js
+++ webpack_demo_app/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
@@ -6,6 +8,10 @@ module.exports = {
         filename: "main.[contentHash].js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })
+    ],
     module: {
         rules: [
             {
