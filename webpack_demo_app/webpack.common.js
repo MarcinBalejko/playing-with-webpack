3cd7f6bd9fe323f85fe883de03756bf1921ff19a
MarcinBalejko <mbbm9@o2.pl>
2020-02-12 17:04:39 +0100

    :white_check_mark: Added html-loader, file-loader, clean-webpack-plugin

--- webpack_demo_app/webpack.common.js
+++ webpack_demo_app/webpack.common.js
@@ -16,6 +16,21 @@ module.exports = {
                     "css-loader",
                     "sass-loader"
                 ]
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        esModule: false,
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
             }
         ]
     }
