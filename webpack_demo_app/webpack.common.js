a76d6e49f465e8149da7d187bacabe548e035be4
MarcinBalejko <mbbm9@o2.pl>
2020-02-12 17:33:00 +0100

    :white_check_mark: Added entrypoint for vendor.js file, added bootstrap.js

--- webpack_demo_app/webpack.common.js
+++ webpack_demo_app/webpack.common.js
@@ -2,10 +2,14 @@ const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/template.html"
-    })
+    entry: {
+        main: "./src/index.js",
+        vendor: "./src/vendor.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })
     ],
     module: {
         rules: [
